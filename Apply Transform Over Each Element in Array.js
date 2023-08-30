ad9e3fc4053fedb8a40c0b5175b778d1c6f7e2e6
Shweta-dogra <shweta6933@gmail.com>
2023-08-30 12:59:27 +0530

    functions

---
+++ Apply Transform Over Each Element in Array.js
@@ -0,0 +1,23 @@
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+// Input: arr = [1,2,3], fn = function plusI(n, i) { return n + i; }
+// Output: [1,3,5]
+// Explanation: The function increases each value by the index it resides in.
+
+
+//Solution
+
+// /**
+//  * @param {number[]} arr
+//  * @param {Function} fn
+//  * @return {number[]}
+//  */
+var map = function(arr, fn) {
+    let mapArr = arr.map((ele, i)=>fn(ele, i));
+    return mapArr;
+    
+};
